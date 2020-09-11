f1a375619127269019e861894f8313f3d773a631
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2020-09-11 07:10:54 -0400

    Template with dynamic data

--- front/subscriber.py
+++ front/subscriber.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    msg = 'Hello World!'
+    return render_template('subscriber.html', msg=msg)
